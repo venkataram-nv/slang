bd8440aef8a67615760d1ede956f4b30051e235a
Venkataram Sivaram <vedavamadath@nvidia.com>
2024-07-12 16:53:34 -0700

    Adding timings to compilation script

--- tests/mdl/compile-modules.sh
+++ tests/mdl/compile-modules.sh
@@ -7,17 +7,23 @@ files=""
 for file in *.slang; do
     if [ -f "$file" ]; then
 		echo "file: $file"
-		slangc $file -o modules/$file-module
+		time slangc $file -o modules/$file-module
 		list="modules/$file-module $list"
 		files="modules/$file-module
-		$files"
+$files"
 	fi
 done
 
 echo "
 compiling altogether:
-		$files"
-slangc $list -target spirv -o dxr.spirv
+
+$files"
+# time slangc $list -target spirv -o dxr.spirv
+time slangc $list -target dxil -o dxr.dxil
+
+echo "
+monolithic compilation:"
+time slangc hit.slang -target dxil -o dxr-monolothic.dxil
 
 read -p "dump spirv-asm? (y/n): " confirm
 if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
